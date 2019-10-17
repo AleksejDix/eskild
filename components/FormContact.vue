b5944d55a4ad431bd636ae1176b5308db63e539b
Aleksej <git@aleksejdix.com>
2019-10-17 22:27:08 +0200

    add element names

--- components/FormContact.vue
+++ components/FormContact.vue
@@ -12,27 +12,27 @@
 
     <label class="block">
       <span class="text-gray-700">Name</span>
-      <input v-model="form.name" class="form-input mt-1 block w-full" placeholder="Max Musterman" required autocomplete="name">
+      <input name="name" v-model="form.name" class="form-input mt-1 block w-full" placeholder="Max Musterman" required autocomplete="name">
     </label>
 
     <label class="block">
       <span class="text-gray-700">E-Mail</span>
-      <input v-model="form.email" type="email" class="form-input mt-1 block w-full" placeholder="max@muster.com"  autocomplete="email">
+      <input name="email" v-model="form.email" type="email" class="form-input mt-1 block w-full" placeholder="max@muster.com"  autocomplete="email">
     </label>
 
     <label class="block">
       <span class="text-gray-700">Telefon</span>
-      <input v-model="form.tel" type="tel"  class="form-input mt-1 block w-full" placeholder="+4952816218444" required autocomplete="tel">
+      <input name="tel" v-model="form.tel" type="tel"  class="form-input mt-1 block w-full" placeholder="+4952816218444" required autocomplete="tel">
     </label>
 
     <label class="block">
       <span class="text-gray-700">Nachricht</span>
-      <textarea v-model="form.text" class="form-textarea mt-1 block w-full" rows="3" placeholder="Ihr wunschtermin"></textarea>
+      <textarea name="text" v-model="form.text" class="form-textarea mt-1 block w-full" rows="3" placeholder="Ihr wunschtermin"></textarea>
     </label>
 
     <div class="flex mt-6">
       <label class="flex items-center">
-        <input v-model="form.protection" type="checkbox" class="form-checkbox">
+        <input name="protection" v-model="form.protection" type="checkbox" class="form-checkbox">
         <span class="ml-2">Ich stimme der <span class="underline">Datenschuzerklärung</span> zu.</span>
       </label>
     </div>
