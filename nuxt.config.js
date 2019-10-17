c5adbb82e97c61cceb7579156c2c013b27f3eebc
Aleksej <git@aleksejdix.com>
2019-10-17 13:08:25 +0200

    add netlify script

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Quicksand&display=swap' }
