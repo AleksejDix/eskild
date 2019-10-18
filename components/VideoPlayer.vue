67c549ae5359634e93950c69eb3a413e4cac6773
Aleksej <git@aleksejdix.com>
2019-10-18 18:48:07 +0200

    add video

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full relative overflow-hidden rounded-lg" :style="{paddingTop: '56.5%'}">
+    <iframe
+      loading="lazy"
+      class="w-full h-full absolute inset-0"
+      type="text/html"
+      allowfullscreen="allowfullscreen"
+      :src="video"
+      frameborder="0"
+    />
+   </div>
+</template>
+
+<script>
+export default {
+  props: ['video']
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <h1 class="title">Podologische Praxis E. Sörensen</h1>
     <hr class="border-t-4 border-teal-500 rounded my-6">
 
+    <VideoPlayer :video="`https://www.youtube.com/embed/fhcV6G_Svw4?modestbranding=1&showinfo=0`"/>
+
+
     <ul v-if="posts.length > 0" class="owl-y">
       <li v-for="post in posts" :key="post.slug" class="py-6">
         <PostPreview :post="post"/>
@@ -14,12 +17,14 @@
 <script>
 
 import Logo from '~/components/Logo'
+import VideoPlayer from '~/components/VideoPlayer'
 import PostPreview from '~/components/PostPreview'
 
 export default {
   components: {
     Logo,
-    PostPreview
+    PostPreview,
+    VideoPlayer
   },
   async asyncData() {
     const files = await require.context('~/content/blog/posts/', false, /\.json$/)
